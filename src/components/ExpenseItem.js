b585ba51042d3032dd4346a93d7248eeb7e80e1c
mohamim360 <mohamim360@gmail.com>
2023-02-08 02:33:01 +0600

    complex component plus css

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,5 +1,14 @@
+import "./ExpenseItem.css";
 
-function ExpenseItem(){
-  return <h2>Expense item</h2>
+function ExpenseItem() {
+  return (
+    <div className="expense-item">
+      <div>March 10th 2020</div>
+      <div className="expense-item__description">
+        <h2>Expense item</h2>
+        <p className="expense-item__price">$190</p>
+      </div>
+    </div>
+  );
 }
 export default ExpenseItem;

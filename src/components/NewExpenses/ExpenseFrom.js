cbd71df4d7f12b030f0b2297eb00e79bbecb60db
mohamim360 <mohamim360@gmail.com>
2023-02-09 02:40:20 +0600

    two way binding in form using state

--- src/components/NewExpenses/ExpenseFrom.js
+++ src/components/NewExpenses/ExpenseFrom.js
@@ -1,45 +1,64 @@
 import React, { useState } from "react";
 import "./ExpenseFrom.css";
+
 const ExpenseFrom = (props) => {
+
   const [enteredTitle, setEnteredTitle] = useState("");
   const [enteredAmount, setEnteredAmount] = useState("");
   const [enteredDate, setEnteredDate] = useState("");
 
   const titleChangeHandler = (event) => {
+    
     setEnteredTitle(event.target.value);
   };
-
-  const amountChangeHandler = (e) =>{
+  const amountChangeHandler = (e) => {
     setEnteredAmount(e.target.value);
-  }
-
-  const dateChangeHandler = (e) =>{
+  };
+  const dateChangeHandler = (e) => {
     setEnteredDate(e.target.value);
-  }
+  };
 
-  const submitHandler = (e) =>{
+  const submitHandler = (e) => {
     e.preventDefault();
     const expenseData = {
       title: enteredTitle,
       amount: enteredAmount,
-      date: new Date(enteredDate) 
-    }
+      date: new Date(enteredDate),
+    };
     console.log(expenseData);
-  }
+    setEnteredTitle("");
+    setEnteredAmount("");
+    setEnteredDate("");
+
+  };
+
+  // return
   return (
     <form onSubmit={submitHandler}>
       <div className="new-expense__controls">
         <div className="new-expense__control">
           <label>Title</label>
-          <input type="text" onInput={titleChangeHandler} />
+          <input type="text" value={enteredTitle} onInput={titleChangeHandler} />
         </div>
         <div className="new-expense__control">
           <label>Amount</label>
-          <input type="number" min="0.01" step="0.01" onInput={amountChangeHandler} />
+          <input
+            type="number"
+            min="0.01"
+            step="0.01" value={enteredAmount}
+            onInput={amountChangeHandler}
+          />
         </div>
         <div className="new-expense__control">
           <label>Date</label>
-          <input type="date" min="2019-01-01" max="2022-12-31" onInput={dateChangeHandler} />
+          <input
+            type="date"
+            min="2019-01-01"
+            max="2022-12-31" value={enteredDate}
+            onInput={dateChangeHandler}
+          />
+           <p>Input value: {enteredTitle}</p>
+  
         </div>
       </div>
       <div className="new-expense__actions">
